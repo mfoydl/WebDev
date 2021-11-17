c85b6c8b35f425e56905863ec9a051fca7ced0d6
mfoydl <milesfoydl@gmail.com>
2021-11-17 13:38:27 -0600

    minor updates

--- script.js
+++ script.js
@@ -24,8 +24,8 @@ function applyRandomColor(event){
 
 function generateRandomColor(){
     let letters = '0123456789ABCDEF';
-    var color = '#';
-    for (var i = 0; i < 6; i++) {
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
         color += letters[Math.floor(Math.random() * 16)];
     }
     return color;
